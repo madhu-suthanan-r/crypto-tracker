777ff4f168dcaec207e6b2b1a9c90ddd699e0023
Madhusuthanan <madhuraj2211@gmail.com>
2024-12-15 17:33:59 +0530

    Crypto-hunter project finished

---
+++ src/component/Header.js
@@ -0,0 +1,72 @@
+import {
+  AppBar,
+  Container,
+  createTheme,
+  makeStyles,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const { currency, setCurrency } = CryptoState();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Hunter
+            </Typography>
+
+            <Select
+             variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
